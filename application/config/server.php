c6812acddaec98421a9b37f04e0a4943210a7050
dhoni <dhoni.p.saputra@gmail.com>
2017-04-27 20:30:10 +0700

    add curl

--- application/config/server.php
+++ application/config/server.php
@@ -1,8 +1,3 @@
 <?php
-$server["processing_server"] = "http://localhost/projects/blog/server_sudo";
-$server["server_url"] = "http://localhost/projects/blog/server/";
-$server["hostname"] = "localhost";
-$server["username"] = "root";
-$server["password"] = "toor";
-$server["database"] = "blog_1";
-$server["blog_key"] = "1tWi2QPIlo5V1HDSvG/8ZOK7bVc/7CJGVPIqOLSKfYAc0jD5vzJ6.";
+$server["processing_server"] = "http://server.sudo.projectblog.laboratorium.xyz";
+
