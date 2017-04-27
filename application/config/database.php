08674b8a9628cf41674d9f519bdfd401b84aabe6
dhoni <dhoni.p.saputra@gmail.com>
2017-04-27 20:27:57 +0700

    Major Update

--- application/config/database.php
+++ application/config/database.php
@@ -62,7 +62,7 @@ $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
+$db['default']['autoinit'] = isset($server['hostname'])? TRUE : FALSE;
 $db['default']['stricton'] = FALSE;
 
 
--- application/controllers/install.php
+++ application/controllers/install.php
@@ -26,6 +26,7 @@ class Install extends CI_Controller {
 
 	public function process_save_settings_database()
 	{
+		$this->load->library('curl');
 		include(APPPATH.'config/server.php');
 		require_once(APPPATH.'libraries/profiling/Pengguna.php');
 		$this->auth = new Pengguna;
@@ -34,12 +35,18 @@ class Install extends CI_Controller {
 		$uniqid = uniqid();
 		$owner_id = $post['owner_id'];
 		
-		$url_blog_key_auth = $server['processing_server'].'/blog/setting_db?token='.$post['blog_key'].'&id='.$owner_id.'&time='.$uniqid.'&u='.urlencode(base_url());
-		$url_blog_key_auth = file_get_contents($url_blog_key_auth);
-		$url_blog_key_auth = json_decode($url_blog_key_auth, true);
+		$data_curl = array(
+				'token' => $post['blog_key'],
+				'id' => $owner_id,
+				'time' => $uniqid,
+				'u' => base_url(),
+				'db' => $post,
+			);
+		$url_curl = $server['processing_server'].'/blog/setting_db?using_auth=0';
 		
 		$connected = $this->authentication->set_db($post)->initialize();
-		if($connected && $url_blog_key_auth['code'] == 200)
+		
+		if($connected)
 		{
 			$text = '<?php'."\n";
 			$text .= '$server["processing_server"] = "'.$server["processing_server"].'";'."\n";
@@ -52,10 +59,40 @@ class Install extends CI_Controller {
 			$text .='$server["blog_key"] = "'.$post['blog_key'].'";'."\n";
 
 			file_put_contents(APPPATH.'config/server.php', $text);
+			$this->curl->simple_post($url_curl, $data_curl); 
+
+			$this->curl->simple_post('install/install_database');
 		}else
 		{
 			header('http/1.0 500 Error on Configuration database');
 		}
+	}
+
+	public function install_database()
+	{
+		// Temporary variable, used to store current query
+		$templine = '';
+		// Read in entire file
+		$lines = file(base_url('locker/database/default.sql'));
+		// Loop through each line
+		foreach ($lines as $line)
+		{
+			// Skip it if it's a comment
+			if (substr($line, 0, 2) == '--' || $line == '')
+			    continue;
+
+			// Add this line to the current segment
+			$templine .= $line;
+			// If it has a semicolon at the end, it's the end of the query
+			if (substr(trim($line), -1, 1) == ';')
+			{
+			    // Perform the query
+			    $this->db->query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+			    // Reset temp variable to empty
+			    $templine = '';
+			}
+		}
+		
 	}
 
 	public function done(){
